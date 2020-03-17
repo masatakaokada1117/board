fe47e61863e647cb26027c0c0d825ea910e37cf8
Masataka Okada <masataka.okada1117@gmail.com>
2020-03-17 13:50:21 +0900

    コメントの削除機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
       flash[:notice] = "コメントを投稿しました"
       redirect_to comment.board
     else
-      redirect_to :back, flash: {
+      redirect_back fallback_location: boards_path, flash: {
         comment: comment,
         error_messages: comment.errors.full_messages
       }
@@ -13,6 +13,9 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    comment = Comment.find(params[:id])
+    comment.delete
+    redirect_to comment.board, flash: { notice: "コメントが削除されました" }
   end
 
   private
